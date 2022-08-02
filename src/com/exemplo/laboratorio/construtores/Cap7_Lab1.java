7dc8ec66aa3b83f67ecacca82142794a278b8570
Daaypeite <daiane.peite@hotmail.com>
2022-08-02 10:44:30 -0300

    first commit

---
+++ src/com/exemplo/laboratorio/construtores/Cap7_Lab1.java
@@ -0,0 +1,15 @@
+package com.exemplo.laboratorio.construtores;
+
+public class Cap7_Lab1 {
+
+	public static void main(String[] args) {
+		Cadastro c1 = new Cadastro();
+		Cadastro c2 = new Cadastro("Claudio ", "Abreu ");
+		Cadastro c3 = new Cadastro("Lourdes ", "Souza ", 40);
+		
+		c1.mostrar();
+		c2.mostrar();
+		c3.mostrar();
+	}
+
+}
